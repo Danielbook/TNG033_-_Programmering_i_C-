72201c8e3cedc38fbee9e1c9cb051a8bbc96a0cc
Daniel Böök <Daniel.book@hotmail.com>
2014-11-25 13:57:20 +0100

    Added Lesson 2

---
+++ Lessons/Lesson2/Exercise1/set.h
@@ -0,0 +1,29 @@
+/***************************************************************************
+* TNG033, Lesson 2                                                         *
+* Defines a class Set represented as an array of booleans                  *
+* header file set.h                                                        *
+* **************************************************************************/
+
+
+#ifndef SET_H_INCLUDED
+#define SET_H_INCLUDED
+
+#include <iostream>
+
+using namespace std;
+
+class Set
+{
+    public:
+
+        //ADD CODE
+
+
+
+    private:
+
+        //ADD CODE
+
+};
+
+#endif // SET_H_INCLUDED
